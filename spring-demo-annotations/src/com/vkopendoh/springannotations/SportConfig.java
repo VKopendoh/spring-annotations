efefce28b8965caa07c5edca8383488437ddae8a
vkopendoh <vkopendoh@bk.ru>
2019-09-07 16:45:49 +0300

    Spring configuration with java code

---
+++ spring-demo-annotations/src/com/vkopendoh/springannotations/SportConfig.java
@@ -0,0 +1,10 @@
+package com.vkopendoh.springannotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.vkopendoh.springannotations")
+public class SportConfig {
+
+}
